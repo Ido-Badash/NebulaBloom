00da4e2213186102fbf72c5d0f024d6183e51731
Ido-Badash <idoba12012011@gmail.com>
2025-03-11 23:28:39 +0200

    🎉 Initial commit, template 📝

---
+++ app/src/main.py
@@ -0,0 +1,28 @@
+import sys
+import logging
+
+def catch_it(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
+            sys.exit(1)
+    return wrapper
+
+@catch_it
+def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S")
+    
+    logging.info("Program started")
+
+    # Run app here
+
+    logging.info("Program ended")
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
